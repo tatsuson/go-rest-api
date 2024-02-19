afde656545dd6b6c90596b3682cdc9dfc3d74847
西口達樹 <tatsuson@nishikoutatsujunoMacBook-Pro.local>
2024-02-20 01:04:44 +0900

    go api fin

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"go-rest-api/controller"
+	"go-rest-api/db"
+	"go-rest-api/repository"
+	"go-rest-api/router"
+	"go-rest-api/usecase"
+	"go-rest-api/validator"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	db := db.NewDB()
+	userValidator := validator.NewUserValidator()
+	taskValidator := validator.NewTaskValidator()
+	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
+	userUsecase := usecase.NewUserUsecase(userRepository, userValidator)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository, taskValidator)
+	userController := controller.NewUserController(userUsecase)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
+	e.Logger.Fatal(e.Start(":8080"))
 }
